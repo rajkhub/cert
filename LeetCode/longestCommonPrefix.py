cd48548fff5b00ea1cd54b505730eba5e76775f5
rajkhub <rskalamata@outlook.com>
2020-06-25 22:40:57 -0500

    Update longestCommonPrefix.py

--- LeetCode/longestCommonPrefix.py
+++ LeetCode/longestCommonPrefix.py
@@ -1,5 +1,12 @@
 #Input: ["flower","flow","flight"]
 #Output: "fl"
+# Zip Returns
+# returns
+#         ('f', 'f', 'f')
+#         ('l', 'l', 'l')
+#         ('o', 'o', 'i')
+#         ('w', 'w', 'g')
+#         ('e', 'e', 'h')
 def longestCommonPrefix(self, strs):
     prefix = []
     for ele in zip(*strs):
