4dcfbd84667acda4f8d93c4078f59cdfa8e7d2de
rajkhub <rskalamata@outlook.com>
2020-07-05 20:25:45 -0500

    Update RemoveDuplicatesfromSortedArray.py

--- LeetCode/RemoveDuplicatesfromSortedArray.py
+++ LeetCode/RemoveDuplicatesfromSortedArray.py
@@ -1,11 +1,11 @@
 #Given nums = [1,1,2],
 #Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
 
-def removeDuplicates(self, nums):
-    i = 0
-    j = 1
-    for j in range(1,len(nums)):
-        if(nums[i] != nums[j]):
-            i += 1
-            nums[i] =nums[j]
+def removeDuplicates(nums):
+    if len(nums)==1: return 1
+    j = 1  #slow pointer
+    for i in range(1,len(nums)): #Fast Pointer
+        if(nums[i] != nums[i-1]):
+            nums[j] =nums[i]
+            j += 1
     return j
