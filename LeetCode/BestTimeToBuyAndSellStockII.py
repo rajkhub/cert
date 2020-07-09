c19a2a61151e8e4f281e51a0b9b17178ae0693a5
rajkhub <rskalamata@outlook.com>
2020-07-08 19:19:07 -0500

    Create BestTimeToBuyAndSellStockII.py

---
+++ LeetCode/BestTimeToBuyAndSellStockII.py
@@ -0,0 +1,12 @@
+#Input: [7,1,5,3,6,4]
+#Output: 7
+#Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+#             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+class Solution(object):
+    def maxProfit(self, prices):  
+        profit =0 
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                profit += prices[i]- prices[i-1]
+        return profit
