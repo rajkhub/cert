c8ccbf5bc2c0e565a5d1e66bd18894eb72258932
rajkhub <rskalamata@outlook.com>
2020-07-19 04:05:30 -0500

    Update JumpGame.py

--- LeetCode/JumpGame.py
+++ LeetCode/JumpGame.py
@@ -2,6 +2,16 @@
 #Output: true
 #Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
 
+#1st Soln
+def canJump(self, nums):
+    jumplen = 0
+    for i in range(len(nums)):
+        if i>jumplen:
+            return False
+        jumplen = max(nums[i]+i, jumplen)
+    return True
+
+#2nd Soln
 def canJump(nums):
     if len(nums) ==1:
         return True
