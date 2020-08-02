469a516fae3ea2c4b9e6ac795edd359a63725a47
rajkhub <rskalamata@outlook.com>
2020-08-02 08:14:10 -0500

    Update LongestSubstringWithRepeatingChar.py

--- LeetCode/LongestSubstringWithRepeatingChar.py
+++ LeetCode/LongestSubstringWithRepeatingChar.py
@@ -6,8 +6,8 @@
 def longestSubstring(s, k):
     for c in set(s):
         if s.count(c) < k:
-            for ele in s.split(c):
-                res =0 
+            res =0
+            for ele in s.split(c): 
                 print ele
                 res =  max(res,longestSubstring(ele,k))
             return res
