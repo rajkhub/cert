9fec38fda75afe1c5cb3b9211f4f3dd04e523058
rajkhub <rskalamata@outlook.com>
2020-08-06 02:48:57 -0500

    Update MergeSortedArray.py

--- LeetCode/MergeSortedArray.py
+++ LeetCode/MergeSortedArray.py
@@ -7,3 +7,19 @@ class Solution(object):
     def merge(self, nums1, m, nums2, n):
         nums1[:] = sorted(nums1[:m] + nums2)
         return nums1
+    
+    
+class Solution(object):
+    def merge(self, nums1, m, nums2, n): 
+        # nums1[:] = sorted(nums1[:m] + nums2)
+        # return nums1
+        while m >0 and n >0:
+            if nums1[m-1]> nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -=1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -=1
+            
+        if n >0:
+            nums1[:n] = nums2[:n]
