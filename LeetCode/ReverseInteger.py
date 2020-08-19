bb52defef6c36b111005cc39bd814c53a1eb4375
rajkhub <rskalamata@outlook.com>
2020-08-19 15:56:15 -0700

    Update ReverseInteger.py

--- LeetCode/ReverseInteger.py
+++ LeetCode/ReverseInteger.py
@@ -2,6 +2,7 @@
 #out = 321
 #In = 120
 #out = 21
+
 def reverse(self, x):
     if x>=0:
         x = int(str(x)[::-1])
@@ -9,3 +10,12 @@ def reverse(self, x):
         print str(-x)
         x= -int(str(-x)[::-1])
     return x if x<=(2**31-1) and x>=-(2**31) else 0 
+
+
+def reverse(number):
+    rev = 0
+    while number>0:
+        n = number%10
+        rev = rev*10 + n
+        number = number//10
+    return rev
