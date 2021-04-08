be357349539c9bef29626578a67accfa742cfa4a
rajkhub <rskalamata@outlook.com>
2021-04-08 00:58:20 -0500

    Create ThreeSum.py

---
+++ LeetCode/ThreeSum.py
@@ -0,0 +1,27 @@
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Notice that the solution set must not contain duplicate triplets.
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+
+
+class Solution(object):
+    def threeSum(self, nums):
+        num = sorted(nums)
+        res =[]
+        for i,a  in enumerate(num):
+            if num[i] == num[i-1] and  i >0:
+                continue
+            l=i+1
+            r= len(num)-1
+            while l<r:
+                threesum = a + num[l] + num[r]
+                if threesum>0:
+                    r-=1
+                elif threesum<0:
+                    l +=1
+                else:
+                    res.append([a,num[l],num[r]])
+                    l +=1
+                    while num[l] == num[l-1] and l <r:
+                        l +=1
+        return res
