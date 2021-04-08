e7579e78db95c5808a8345d318a73c83831e77cd
rajkhub <rskalamata@outlook.com>
2021-04-08 01:40:59 -0500

    Update MergeTwoSortedLists.py

--- LeetCode/MergeTwoSortedLists.py
+++ LeetCode/MergeTwoSortedLists.py
@@ -5,6 +5,8 @@
 #         self.next = next
 #Input: 1->2->4, 1->3->4
 #Output: 1->1->2->3->4->4
+# Input: l1 = [], l2 = [0]
+# Output: [0]
 
 class Solution(object):
     def mergeTwoLists(self, l1, l2):
@@ -24,3 +26,22 @@ class Solution(object):
         if l2 is None:
             currNode.next = l1
         return dummy.next
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next,l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
