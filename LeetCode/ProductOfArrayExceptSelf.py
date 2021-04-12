62f266ef2b78189f1d0b0de0b91c174462937c15
rajkhub <rskalamata@outlook.com>
2021-04-12 09:23:08 -0500

    Update ProductOfArrayExceptSelf.py

--- LeetCode/ProductOfArrayExceptSelf.py
+++ LeetCode/ProductOfArrayExceptSelf.py
@@ -2,6 +2,24 @@
 #input =[1,2,3,4]
 # Output = [24, 12, 8, 6]
 arr=[1,2,3,4]
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        ans=[0]*len(nums)
+        ans[0]=1
+        for i in range(1,len(nums)):
+            ans[i] = ans[i-1] * nums[i-1]
+        
+        R=1
+        for i in reversed(range(len(nums))):
+            ans[i]= ans[i]*R
+            R *= nums[i]
+        return ans
+
+
+
+
 def multiply(arr):
     res = []
     for i in range(len(arr)):
