3c1c49d071309181470467f537f13bd12e92cd75
rajkhub <rskalamata@outlook.com>
2021-04-11 19:35:08 -0500

    Update SearchInRotatedSortedArray.py

--- LeetCode/SearchInRotatedSortedArray.py
+++ LeetCode/SearchInRotatedSortedArray.py
@@ -1,6 +1,33 @@
 #Input: nums = [4,5,6,7,0,1,2], target = 0
 #Output: 4
  
+
+ class Solution(object):
+    def search(self, nums, target):
+        if not nums:
+            return -1
+        low = 0 
+        high = len(nums)-1
+        
+        while low <= high:
+            mid = (low + high)//2
+            
+            if target == nums[mid]:
+                return mid
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid -1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid+1
+                else:
+                    high = mid -1
+        return -1
+ 
+ 
+ 
  def search(self, nums, target):
      first = 0
      last = len(nums)-1
