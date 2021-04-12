703779ff9c2ee7870ce363816766c9b31bd19187
rajkhub <rskalamata@outlook.com>
2021-04-12 07:11:45 -0500

    Update ValidPalindrome.py

--- LeetCode/ValidPalindrome.py
+++ LeetCode/ValidPalindrome.py
@@ -1,6 +1,24 @@
 #Input: "A man, a plan, a canal: Panama"
 #Output: true
 
+class Solution(object):
+    def isPalindrome(self, s):
+        l =0
+        r = len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l +=1
+            while l < r and not s[r].isalnum():
+                 r -=1
+            
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l +=1
+            r -=1
+        return True
+
+
 import re
 class Solution(object):
     def isPalindrome(self, s):
