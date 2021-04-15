d56667e0537179902e62e14a1d2f26ee199227b9
rajkhub <rskalamata@outlook.com>
2021-04-15 15:57:26 -0500

    Update BinaryTreeHeight.py

--- LeetCode/BinaryTreeHeight.py
+++ LeetCode/BinaryTreeHeight.py
@@ -1,3 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root,cnt = 0):
+        if root == None:
+            return cnt
+        cnt +=1
+        lft = self.maxDepth(root.left,cnt)
+        rght = self.maxDepth(root.right,cnt)
+        return max(lft,rght)
+
+        
+        
+class Solution(object):
+    def maxDepth(self, root ):
+        if root == None:
+            return 0
+        else:
+            lft = self.maxDepth(root.left)
+            rght = self.maxDepth(root.right)
+            return max(lft,rght) + 1 
+ 
+ 
  def maxDepth(self, root):
      if not root:
          return 0
